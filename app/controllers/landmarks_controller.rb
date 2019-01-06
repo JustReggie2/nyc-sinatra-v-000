975ab4ee36010b149c80c12bc5c0cf5420177218
Reginald T Carter iI <reginaldcarterii@gmail.com>
2019-01-06 19:23:47 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,4 +8,11 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.save
+
+    redirect "landmarks/#{@landmark.id}"
+  end
 end
